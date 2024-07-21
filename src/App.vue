553bdab98964da8dbf1539b9ab23d7389bb23afa
hzyycom <hzyycom@163.com>
2024-07-21 11:01:43 +0800

    直接首页，不加导航

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <head>
-    <title> 财经速递 </title>
+    <title> 聚搜通义 </title>
   </head>
 
   <div>
-    <!--使用 router-link 组件进行导航 -->
-    <!--通过传递 `to` 来指定链接 -->
-    <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+
+    <!-- <RouterLink to="/">首页</RouterLink>
+    <RouterLink to="/about"> about </RouterLink> -->
+   
+  <MeiliSearch host="https://weipaitang.online/search"></MeiliSearch>
 
 
-    
 
-    <RouterLink to="/">首页</RouterLink>
-    <!-- <RouterLink to="/finance"> 财经</RouterLink> -->
-    <RouterLink to="/about"> about </RouterLink>
+
   </div>
 
   <!-- 路由出口 -->
@@ -24,14 +23,15 @@
 </template>
 
 <script>
+import MeiliSearch from "../components/MeiliSearch.vue";
+// import Menu from "../components/Menu";
 
-// var _hmt = _hmt || [];
-// (function() {
-//   var hm = document.createElement("script");
-//   hm.src = "https://hm.baidu.com/hm.js?8f5031152a68024ef140e21769b9f523";
-//   var s = document.getElementsByTagName("script")[0]; 
-//   s.parentNode.insertBefore(hm, s);
-// })();
+export default {
+  name: "App",
+  components: {
+    MeiliSearch
+  },
+};
 </script>
 
 
---
+++ src/App2.vue
@@ -0,0 +1,38 @@
+<template>
+    <head>
+      <title> 财经速递 </title>
+    </head>
+  
+    <div>
+      <!--使用 router-link 组件进行导航 -->
+      <!--通过传递 `to` 来指定链接 -->
+      <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+  
+  
+      
+  
+      <RouterLink to="/">首页</RouterLink>
+      <!-- <RouterLink to="/finance"> 财经</RouterLink> -->
+      <RouterLink to="/about"> about </RouterLink>
+    </div>
+  
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  
+  
+  </template>
+  
+  <script>
+  
+  // var _hmt = _hmt || [];
+  // (function() {
+  //   var hm = document.createElement("script");
+  //   hm.src = "https://hm.baidu.com/hm.js?8f5031152a68024ef140e21769b9f523";
+  //   var s = document.getElementsByTagName("script")[0]; 
+  //   s.parentNode.insertBefore(hm, s);
+  // })();
+  </script>
+  
+  
+  
